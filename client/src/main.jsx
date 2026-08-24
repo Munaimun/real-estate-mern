fefe062a3dbf42c6f63e66ae5b054d4b2829c021
munaimun <munaimunz@gmail.com>
2026-08-25 02:32:31 +0600

    added redux pesist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,18 @@
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
 
-import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { persistor, store } from "./redux/store.js";
 
 import "./index.css";
+
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loadin={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,47 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+import { persistReducer, persistStore } from "redux-persist";
 
 import userReducer from "./user/userSlice";
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer, // Add the user reducer to the Redux store.
+const storage = {
+  getItem: (key) => Promise.resolve(localStorage.getItem(key)),
+  setItem: (key, value) => {
+    localStorage.setItem(key, value);
+    return Promise.resolve();
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }),
+  removeItem: (key) => {
+    localStorage.removeItem(key);
+    return Promise.resolve();
+  },
+};
+
+// Combine all reducers into one main reducer.
+const rootReducer = combineReducers({
+  user: userReducer,
 });
+
+// Tell Redux Persist how to save the Redux state.
+const persistConfig = {
+  key: "root", // Name used to store the saved Redux state.
+  storage, // Save the state in the browser's localStorage.
+  version: 1, // Version of the saved state.
+};
+
+// Add persistence to the root reducer.
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create the Redux store.
+export const store = configureStore({
+  // Use the persisted reducer so Redux state can be saved and restored.
+  reducer: persistedReducer,
+
+  // Use Redux Toolkit's default middleware.
+  // Turn off the serializable value check because of Redux Persist.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store); // Create a persistor to manage saving and restoring the Redux state.
