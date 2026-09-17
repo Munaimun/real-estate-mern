af52705b8a6bd3fd72fb1ea44646221207cadc28
munaimun <munaimunz@gmail.com>
2026-09-18 04:02:39 +0600

    add: search page ui

---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,77 @@
+const Search = () => {
+  return (
+    <div className="flex flex-col md:flex-row gap-5">
+      {/* left side = the form */}
+      <div className="p-5 border-slate-200 border-b-2 md:border-r-2 md:min-h-screen bg-slate-100">
+        <form className="flex flex-col gap-5">
+          <div className="flex items-center gap-2 ">
+            <label className="whitespace-nowrap font-semibold">
+              Search Term:
+            </label>
+            <input
+              type="text"
+              id="searchTerm"
+              placeholder="search..."
+              className="border rounded-lg w-full p-3 bg-white"
+            />
+          </div>
+
+          <div className="flex gap-2 flex-wrap items-center">
+            <label className="font-semibold">Type:</label>
+            <div className="flex gap-2">
+              <input type="checkbox" id="all" className="w-5" />
+              <span>Rent & Sale</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="rent" className="w-5" />
+              <span>Rent</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="sale" className="w-5" />
+              <span>Sale</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="Offer" className="w-5" />
+              <span>Offer</span>
+            </div>
+          </div>
+
+          <div className="flex gap-2 flex-wrap items-center">
+            <label className="font-semibold">Amenities:</label>
+            <div className="flex gap-2">
+              <input type="checkbox" id="parking" className="w-5" />
+              <span>Parking</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="furnished" className="w-5" />
+              <span>Furnished</span>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <label className="font-semibold">Sort:</label>
+            <select id="sort_order" className="border rounded-lg p-3">
+              <option>Price high to low</option>
+              <option>Price low to high</option>
+              <option>Latest</option>
+              <option>Oldest</option>
+            </select>
+          </div>
+
+          <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
+            Search
+          </button>
+        </form>
+      </div>
+
+      {/* right side = the result */}
+      <div className="">
+        <h1 className="text-3xl font-semibold p-3 text-slate-700 sm:mt-2 mt-0">
+          Listing Results:
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
