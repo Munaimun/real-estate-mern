685f85a386f77146aad4837f60dc83aaffaf5dfd
munaimun <munaimunz@gmail.com>
2026-09-21 16:39:15 +0600

    Configure Render deployment

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+import path from "path";
+
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
@@ -18,11 +20,13 @@ mongoose
     console.error("Error connecting to MongoDB:", error);
   });
 
+const __dirname = path.resolve(); // Get the current directory path
+
 const app = express();
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(cookieParser()); // Middleware to parse cookies
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
@@ -32,6 +36,13 @@ app.listen(PORT, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "client", "dist"))); // Serve static files from the React app
+
+// Serve the React app for any other route
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // middleware
 app.use((err, req, res, next) => {
